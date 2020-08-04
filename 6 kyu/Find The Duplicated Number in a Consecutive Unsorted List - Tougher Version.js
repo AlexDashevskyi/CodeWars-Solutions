951f950de5277306aa99f979561310fc1493fc95
Oleksandr Dashevskyi <66935945+AlexDashevskyi@users.noreply.github.com>
2020-08-04 01:03:41 -0700

    Create Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js

---
+++ 6 kyu/Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js
@@ -0,0 +1,10 @@
+{/*
+TASK
+Spin-off of this kata, here you will have to figure out an efficient strategy to solve the problem of finding the sole duplicate number among an unsorted array/list of numbers starting from 1 up to n.
+
+Hints: a solution in linear time can be found; using the most intuitive ones to search for duplicates that can run in O(n²) time won't work.
+*/}
+
+const findDup=function(arr){
+ return arr.filter((el, i) => i === arr.indexOf(el) && i !== arr.lastIndexOf(el))[0];
+}
